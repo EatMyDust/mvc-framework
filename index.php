c769f14fa06650e2b1eeb594cb274b4ff0de2e6b
Oleg Prochanov <prochanov@ukr.net>
2021-02-03 17:09:18 +0200

    Added router and autoload

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
+require_once __DIR__.'/autoload.php';
+use app\core\Application;
+
+$app = new Application();
+$app->router->get('/', function (){
+    return 'hello';
+});
+$app->run();
+
 ?>
